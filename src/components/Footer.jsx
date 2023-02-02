06700d915dc54737e260239c721acb121d37974e
carlos mur <cmurestudillos@gmail.com>
2023-02-02 14:43:59 +0100

    add: Update project

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,7 @@ const Footer = () => {
     return ( 
         <footer className="navbar navbar-light bg-dark fixed-bottom">
             <div className="text-white mx-auto">
-                <i>Copyright © - Designed and Created by: 
-                    <a className="text-white" href="https://www.cmurestudillos.es" target="_blank" rel="noopener noreferrer"> <strong>Carlos Mur</strong> </a>
-                </i>
+                <i>Copyright © - Designed and Created by: <strong>Carlos Mur</strong> </i>
             </div>
         </footer>          
      );
